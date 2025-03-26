37b0707d4886ac53c60f5b23686444013eabcec0
nosovegor178 <nosovbelakovegor01@gmail.com>
2025-03-26 10:47:33 +0300

    Fixed decoration mistakes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 import requests
 from dotenv import load_dotenv
-from pprint import pprint
 import telegram
 import argparse
 
@@ -11,7 +10,7 @@ load_dotenv()
 
 tg_token = os.environ['TG_BOT_TOKEN']
 bot = telegram.Bot(tg_token)
-url='https://dvmn.org/api/long_polling/'
+url = 'https://dvmn.org/api/long_polling/'
 API_TOKEN = os.environ['DEVMAN_TOKEN']
 headers = {
     'Authorization': API_TOKEN
@@ -20,13 +19,13 @@ headers = {
 
 def send_info_about_attempt(attempt, chat_id):
     if attempt['is_negative']:
-        bot.send_message(chat_id, f'''У вас проверили задание с урока 
-{attempt['lesson_title']} 
+        bot.send_message(chat_id, f'''У вас проверили задание с урока
+"{attempt['lesson_title']}"
 ({attempt['lesson_url']})\n
 К сожалению, в работе нашлись ошибки.''')
     else:
-        bot.send_message(chat_id, f'''У вас проверили задание с урока 
-{attempt['lesson_title']} 
+        bot.send_message(chat_id, f'''У вас проверили задание с урока
+"{attempt['lesson_title']}"
 ({attempt['lesson_url']})\n
 Работа принята, можно продолжать!''')
 
@@ -43,14 +42,13 @@ def looking_for_attempts(timestamp, url, headers):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         prog='devman_tg_bot',
-        description='''Send notifications about finishing of 
+        description='''Send notifications about finishing of
         checking in Devman site by Telegram bot''')
     parser.add_argument('chat_id',
                         help='Input your Telegram chat id here')
     args = parser.parse_args()
 
-
-    timestamp=None
+    timestamp = None
     while True:
         try:
             response = looking_for_attempts(timestamp, url, headers)
